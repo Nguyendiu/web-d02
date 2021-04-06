4024f711b2b09af1d37743edd7304e762d4fc5a7
Nguyendiu <nguyendiupro15@gmail.com>
2021-04-06 13:47:40 +0700

    login form

--- lesson-8-jquery/Server/server.js
+++ lesson-8-jquery/Server/server.js
@@ -2,11 +2,18 @@ const express = require('express'); // Include ExpressJS
 const app = express(); // Create an ExpressJS app
 const bodyParser = require('body-parser'); // Middleware 
 
-const users = [{
-    name: 'admin',
-    password: '123'
-}]
-console.log(users.name)
+let users = [
+    {
+        name: 'admin',
+        pass: '123'
+    },
+    {
+        name: 'teo',
+        pass: '456'
+    }
+]
+
+console.log(users[1].pass)
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route to Homepage
@@ -22,11 +29,20 @@ app.post('/login', (req, res) => {
     // Insert Login Code Here
     let username = req.body.username;
     let password = req.body.password;
-    if (username === "admin" && password === "123") {
-        res.send(`Dang nhap thanh cong`);
+    // if (username === "admin" && password === "123") {
+    //     res.send(`Dang nhap thanh cong`);
 
-    } else
-        res.send(`sai tai khoan hoac mat khau`);
+    // } else
+    //     res.send(`sai tai khoan hoac mat khau`);
+    for (let i = 0; i <= users.length; i++) {
+        if (users[i].name === username && users[i].pass === password) {
+            res.send('Login Success')
+
+        } else {
+            res.send('Check user or password')
+
+        }
+    }
 });
 
 const port = 3000 // Port we will listen on
