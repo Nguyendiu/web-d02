af355d8be7751571b729536c6ed24f2159dd884d
Nguyendiu <nguyendiupro15@gmail.com>
2021-04-09 21:32:18 +0700

    done

---
+++ NetWork-app/src/users.router.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    res.send('GET route on thingsssdsd.');
+});
+router.post('/login', function (req, res) {
+    res.send('POST route on things.');
+});
+
+//export this router to use in our index.js
+module.exports = router;
